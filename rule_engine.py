063d7f996c1275d162fe1ba78a78f2674a1f504c
huuhuannt1998 <huuhuannt1998@gmail.com>
2024-04-03 18:57:30 -0400

    update rule engine

---
+++ rule_engine.py
@@ -0,0 +1,35 @@
+import rule_engine
+import datetime
+
+comics = [
+  {
+    'title': 'Batman',
+    'publisher': 'DC',
+    'issue': 89,
+    'released': datetime.date(2020, 4, 28)
+  },
+  {
+    'title': 'Flash',
+    'publisher': 'DC',
+    'issue': 753,
+    'released': datetime.date(2020, 5, 5)
+  },
+  {
+    'title': 'Captain Marvel',
+    'publisher': 'Marvel',
+    'issue': 18,
+    'released': datetime.date(2020, 5, 6)
+  }
+]
+
+
+rule = rule_engine.Rule(
+  # match books published by DC
+  'publisher == "DC"'
+)
+
+# check if the first object matches
+print(rule.matches(comics[0])) # => True
+
+# filter the iterable "comics" and return matching objects
+print(rule.filter(comics)) # => <generator object Rule.filter at 0x7f2bdafbe650>
